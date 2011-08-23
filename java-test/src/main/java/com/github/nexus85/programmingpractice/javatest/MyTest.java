8046482161db86b3b78effd819450a09b862fe61
wbao <wbao@bj-rhel60-dev.(none)>
2011-08-23 11:44:58 +0800

    Initial commit Creating project  Committer: wbao <wbao@bj-rhel60-dev.(none)>  Changes to be committed:    (use "git rm --cached <file>..." to unstage)
    	new file:   README
    	new file:   java-test/.classpath
    	new file:   java-test/.project
    	new file:   java-test/.settings/org.eclipse.jdt.core.prefs
    	new file:   java-test/pom.xml
    	new file:   java-test/src/main/java/com/github/nexus85/programmingpractice/javatest/MyTest.java
    	new file:   pom.xml

---
+++ java-test/src/main/java/com/github/nexus85/programmingpractice/javatest/MyTest.java
@@ -0,0 +1,8 @@
+package com.github.nexus85.programmingpractice.javatest;
+
+public class MyTest {
+
+	public static void main(String[] args){
+		System.out.println("First program in github!");
+	}
+}
