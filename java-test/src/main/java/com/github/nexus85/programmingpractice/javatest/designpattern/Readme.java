fe542eb798f39d68f91a6b506cbcc136d049a259
William Bao <baowenle@gmail.com>
2011-08-23 18:32:01 +0800

    save

---
+++ java-test/src/main/java/com/github/nexus85/programmingpractice/javatest/designpattern/Readme.java
@@ -0,0 +1,25 @@
+/**
+* @file Readme.java
+* 
+* @author wbao
+* 
+* @date Aug 23, 2011
+* 
+* Copyright (C) 2010-2011, Bytemobile, Inc. All rights reserved.
+*/
+
+package com.github.nexus85.programmingpractice.javatest.designpattern;
+
+/**
+ * Prototype pattern ==> java Cloneable
+ * Iterator pattern ==> java Iterator
+ * 
+ * 
+ * 
+ * @author wbao
+ *
+ */
+
+public class Readme {
+
+}
