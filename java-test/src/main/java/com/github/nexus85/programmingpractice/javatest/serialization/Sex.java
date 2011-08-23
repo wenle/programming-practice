1c3090c3c6eba3e367cc36a3e3d8ea63d0404854
William Bao <baowenle@gmail.com>
2011-08-23 15:28:27 +0800

    del target

---
+++ java-test/src/main/java/com/github/nexus85/programmingpractice/javatest/serialization/Sex.java
@@ -0,0 +1,32 @@
+/**
+* @file Aa.java
+* 
+* @author wbao
+* 
+* @date Aug 23, 2011
+* 
+* Copyright (C) 2010-2011, Bytemobile, Inc. All rights reserved.
+*/
+
+package com.github.nexus85.programmingpractice.javatest.serialization;
+
+
+public class Sex {
+
+  private String sex;
+  
+  public Sex() {
+    this.setSex("For Test");
+  }
+
+  public void setSex(String sex) {
+
+    this.sex = sex;
+  }
+
+  public String getSex() {
+
+    return sex;
+  }
+  
+}
